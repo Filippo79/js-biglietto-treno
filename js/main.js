29225cfd3c8c1916e6eec65c04cb6ee9965f36da
Filippo Pittau <filippo.pittau@gmail.com>
2020-02-18 19:52:46 +0100

    Commit 2

--- js/main.js
+++ js/main.js
@@ -3,20 +3,42 @@
 //2 Quanti anni hai ?
 //3 1 km costa 0,21
 //4 Quanto costa il biglietto totale?
-//5 Al prezzo del biglietto calcolare il -20% per i minorenni
-//6 al prezzo del biglietto calcolare il -40% pri gli over 65
+//5 Condizione -20% per i minorenni
+//6 Condizione -40% per gli over 65
 
 //1 Quanti km devi fare?
 var kmDaPercorrere = prompt ('Quanti Km devi percorrere ?');
-document.getElementById ('Km-percorrere').innerHTML = kmDaPercorrere;
+document.getElementById ('km-percorrere').innerHTML = kmDaPercorrere;
 console.log(kmDaPercorrere);
+
 //2 Quanti anni hai ?
 var quantiAnni = prompt ('Quanti anni hai ?');
 document.getElementById ('quanti-anni').innerHTML = quantiAnni;
 console.log(quantiAnni);
+
 //3 1 km costa 0,21
-var km = (0,21);
+var km = (0.21);
 console.log(km);
+
 //4 Quanto costa il biglietto totale?
 var biglietto = (km * kmDaPercorrere);
+document.getElementById ('prezzo-biglietto').innerHTML = biglietto;
 console.log(biglietto);
+
+// mi sono perso qui....... AIUTOOOOOOOOOOOO !!!!!!!!!!!!!!!!!!!!!!!!
+//5 Condizione -20% per i minorenni
+if (quantiAnni < 18){ // calcolo sconto per minori
+    biglietto -= biglietto * 0.2; // non sono riuscito chiesto a Fabiola cosa significa -= perchè???????
+    console.log('Prezzo scontato 20%');
+    document.getElementById ('prezzo-biglietto-scontato').innerHTML = biglietto;
+}
+
+//6 Condizione -40% per gli over 65
+else if (quantiAnni > 65) { // calcolo sconto maggiori 65
+    biglietto -= biglietto * 0.4;// non sono riuscito chiesto a Fabiola cosa significa -= perchè???????
+    console.log('Prezzo scontato 40%');
+    document.getElementById ('prezzo-biglietto-scontato').innerHTML = biglietto;
+}
+else {
+}
+document.getElementById('container').setAttribute('class' , 'visible');
